4647d61ec1d6169707432d1113a092142e43f6ed
DafiNMaulana <dafinmaulana18@gmail.com>
2023-10-29 00:06:08 +0700

    feat:add new component

---
+++ src/components/BtnPrimary.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function BtnPrimary() {
+  return (
+    <div>BtnPrimary</div>
+  )
+}
---
+++ src/components/partials/navbar/LangSwitcher.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function LangSwitcher() {
+  return (
+    <div>LangSwitcher</div>
+  )
+}
