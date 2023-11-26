3550eeac080290bea2e2848a41fb1165c839fc49
DafiNMaulana <dafinmaulana18@gmail.com>
2023-11-27 05:01:40 +0700

    chore:cleansing the code

--- src/components/IsDataFetched.jsx
+++ src/components/IsDataFetched.jsx
@@ -4,13 +4,12 @@ export default function IsDataFetched({isLoading, isError, data}) {
   return (
     <>
       {isLoading && <span className="loading loading-spinner loading-lg mx-auto col-span-3"></span>}
-      {isError ? (
-        <p className="text-center text-accent font-medium tracking-wide col-span-3">Whoops, there&apos;s something wrong 😅</p>
-      ) : data.length == 0 && !isLoading ? (
-        <p className="text-center text-accent font-medium tracking-wide col-span-3">Whoops, the data couldn&apos;t be found. 😅</p>
-      ) : data.length == 0 && isError && !isLoading ? (
-        <p className="text-center text-accent font-medium tracking-wide col-span-3">Whoops, the data couldn&apos;t be found. 😅</p>
-      ) : null}
+
+      {!isLoading && isError && <p className="text-center text-accent font-medium tracking-wide col-span-3">Whoops, there's something wrong 😅</p>}
+
+      {!isLoading && !isError && data.length === 0 && (
+        <p className="text-center text-accent font-medium tracking-wide col-span-3">Whoops, the data couldn't be found. 😅</p>
+      )}
     </>
   );
 }
