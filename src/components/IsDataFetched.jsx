19d4774fa1fb976eb498e8e734ce451981033263
DafiNMaulana <dafinmaulana18@gmail.com>
2023-11-27 05:06:19 +0700

    chore:changed the quotation mark to code

--- src/components/IsDataFetched.jsx
+++ src/components/IsDataFetched.jsx
@@ -5,10 +5,10 @@ export default function IsDataFetched({isLoading, isError, data}) {
     <>
       {isLoading && <span className="loading loading-spinner loading-lg mx-auto col-span-3"></span>}
 
-      {!isLoading && isError && <p className="text-center text-accent font-medium tracking-wide col-span-3">Whoops, there's something wrong 😅</p>}
+      {!isLoading && isError && <p className="text-center text-accent font-medium tracking-wide col-span-3">Whoops, there&apos;s something wrong 😅</p>}
 
       {!isLoading && !isError && data.length === 0 && (
-        <p className="text-center text-accent font-medium tracking-wide col-span-3">Whoops, the data couldn't be found. 😅</p>
+        <p className="text-center text-accent font-medium tracking-wide col-span-3">Whoops, the data couldn&apos;t be found. 😅</p>
       )}
     </>
   );
