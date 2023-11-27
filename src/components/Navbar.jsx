c80f67c6bcc7ddab5bd6640a0433d2e7ba6b4338
DafiNMaulana <dafinmaulana18@gmail.com>
2023-11-27 08:50:40 +0700

    style:remove padding Y

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ import NavbarEnd from "./partials/navbar/NavbarEnd";
 
 export default function Navbar() {
   return (
-    <div className="navbar bg-base-100 container mx-auto py-5 border-b border-accent/20 px-3 fixed top-0 left-0 right-0 z-50">
+    <div className="navbar bg-base-100 container py-2 mx-auto border-b border-accent/20 px-3 fixed top-0 left-0 right-0 z-50">
       <NavbarIcon/>
 
       <Menu/>
