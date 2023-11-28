dea3c84acd1a9288c34e764f0a1331995e943615
DafiNMaulana <dafinmaulana18@gmail.com>
2023-11-29 01:12:05 +0700

    style:if btn outline hovered, change the text color to accent

--- src/components/BacOutlineAccent.jsx
+++ src/components/BacOutlineAccent.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Link from "next/link";
 export default function BacOutlineAccent({href, content, className}) {
   return (
-    <Link href={href} className={`btn btn-outline capitalize px-8 hover:bg-accent text-base ${className}`}>
+    <Link href={href} className={`btn btn-outline capitalize px-8 hover:bg-accent hover:text-base-100 text-base ${className}`}>
       {content}
     </Link>
   );
